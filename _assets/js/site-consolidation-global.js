6c872b738df36ed96c8ece2ce56e83fddd4cc1a5
Aaron Alexander <nerdfiles@gmail.com>
2011-05-13 12:28:20 -0500

    cleanup

--- _assets/js/site-consolidation-global.js
+++ _assets/js/site-consolidation-global.js
@@ -684,7 +684,7 @@ jQuery(document).ready(function() {
                 _src = $lis.eq(c-1).next().find('img').attr('src');
             } else {
                 c = 0;
-                _src = $lis.eq(0).find('img').attr('src');
+                _src = $lis.eq(0).next().find('img').attr('src');
             }
 
             if (crossFade === true) {
